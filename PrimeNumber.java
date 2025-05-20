509fc960c9b7e4d8dddf7e7808c99a5a5e65bb4c
Tanmay Anand <73070862+Tanmay-Anand@users.noreply.github.com>
2025-05-20 16:31:00 +0530

    Add files via upload

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -3,32 +3,29 @@ package tPointTech;
 import java.util.Scanner;
 
 public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Created Scanner
 
-	public static void main(String[] args) {
-		
-		int num;
-		Scanner s = new Scanner(System.in);
-		System.out.print("Enter a number: ");
-		num = s.nextInt();
-		
-		CheckPrimeNumber(num);
-		s.close(); // good practice
-	}
-	
-	public static void CheckPrimeNumber(int num) {
-		
-		boolean isPrime = true;
-		for (int i = 2; i <= num / 2; i++) {
-			if (num % i == 0) {
-				isPrime = false;
-				break;
-			}
-		}
-		
-		if (isPrime) {
-			System.out.println("It is a Prime Number.");
-		} else {
-			System.out.println("It is NOT a Prime Number.");
-		}
-	}
+        System.out.print("Enter a number to check if it's prime: ");
+        int num = scanner.nextInt(); // Read input
+
+        if (isPrime(num)) {
+            System.out.println(num + " is a Prime Number.");
+        } else {
+            System.out.println(num + " is NOT a Prime Number.");
+        }
+
+        scanner.close(); // Close Scanner
+    }
+
+    public static boolean isPrime(int n) {
+        if (n < 2) return false; // 0 and 1 are not prime
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false; // not prime
+            }
+        }
+        return true; // prime
+    }
 }
